c76c2040e5905c0682c6919dbd0b55000b4a6890
oksiklepykova <oksiklepykova@gmail.com>
2022-12-02 15:35:31 +0300

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -33,8 +33,19 @@ function displayTemperature(response){
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "7de8edc4ed814aeb0a4e486a9d398f79";
-let city = "Lviv";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+    let apiKey = "7de8edc4ed814aeb0a4e486a9d398f79";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("Lviv");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
