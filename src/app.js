dac6692c4b8bc742b90794e2f53c55902cd323d8
oksiklepykova <oksiklepykova@gmail.com>
2022-12-06 19:41:35 +0300

    Connected  the Forecast to API

--- src/app.js
+++ src/app.js
@@ -13,7 +13,9 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+    console.log(response.data.daily);
+    
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row">`;
@@ -34,9 +36,15 @@ function displayForecast() {
     
      forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
-    console.log(forecastHTML);
 }
 
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "c8735bb7e8e2f8d8a38c7501f3cd47d3"; // apiKey took SheCodes
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response){
   let temperatureElement = document.querySelector("#temperature");
@@ -57,6 +65,8 @@ function displayTemperature(response){
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
+    
+    getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -101,4 +111,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Lviv");
-displayForecast();
